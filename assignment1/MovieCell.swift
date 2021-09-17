f5956306bd4f85d79b4694c64892fd58e001b827
Nick Vardaro <njv242@nyu.edu>
2021-09-17 17:15:08 -0400

    update with assignment specs

---
+++ assignment1/MovieCell.swift
@@ -0,0 +1,27 @@
+//
+//  MovieCell.swift
+//  assignment1
+//
+//  Created by Nicholas Vardaro on 9/14/21.
+//
+
+import UIKit
+
+class MovieCell: UITableViewCell {
+
+    @IBOutlet weak var titleLabel: UILabel!
+    @IBOutlet weak var synopsisLabel: UILabel!
+    @IBOutlet weak var postView: UIImageView!
+    
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(_ selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
